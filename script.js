337b5e0ed2754359ef2fc00285baf64bac3c3c4c
Msa33 <76279162+Msa33@users.noreply.github.com>
2025-05-03 09:05:44 +0100

    commit add to card

--- script.js
+++ script.js
@@ -14,3 +14,10 @@ function Carrossel() {
   slides[diapoIndex - 1].style.display = "block";
   setTimeout(Carrossel, 3000); // Change d'image toutes les 2 secondes
 }
+
+function addTcard(NomProduit){
+  //alert(NomProduit +" a été ajouté au panier");
+  qte = document.getElementById("quantite");
+  quantite = parseInt(qte.innerHTML);
+  qte.innerHTML = quantite + 1;
+}
